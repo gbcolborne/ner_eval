494c43f1b6edbf778ec87ea9d21e044db40dcb9b
gbcolborne <g.b.colborne@gmail.com>
2019-11-12 14:11:36 -0500

    Write Tensorboard data in output directory

--- exp/run_transformers_ner.py
+++ exp/run_transformers_ner.py
@@ -252,7 +252,7 @@ def set_seed(args):
 def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
     """ Train the model """
     if args.local_rank in [-1, 0]:
-        tb_writer = SummaryWriter()
+        tb_writer = SummaryWriter(logdir=args.output_dir)
 
     args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
     train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
